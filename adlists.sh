aee5fac7e03c7524b9d8aeff25fb6053b0ea25f9
Benjamin Lenau Hansen <benjamin@lenau.dk>
2020-10-28 19:44:25 +0100

    Update adlists.sh
    Added more adlists to the mix.

--- adlists.sh
+++ adlists.sh
@@ -2,6 +2,11 @@
 
 # update pihole
 sqlite3 /etc/pihole/gravity.db "SELECT Address FROM adlist" |sort >pihole.list
+
+# uncomment following wget-lines if you want more list
+#wget -qO - https://v.firebog.net/hosts/lists.php?type=tick |sort >>temp.list
+#wget -qO - https://block.energized.pro/ultimate/formats/hosts.txt |sort >>temp.list
+
 echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" >>temp.list
 echo "https://mirror1.malwaredomains.com/files/justdomains" >>temp.list
 echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" >>temp.list
