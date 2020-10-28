6eb68be395a8ace4b143d4a4f4ce20c57e8bfea3
Benjamin Lenau Hansen <benjamin@lenau.dk>
2020-10-28 17:42:51 +0100

    Update docker_pihole_auto.sh

--- docker_pihole_auto.sh
+++ docker_pihole_auto.sh
@@ -87,7 +87,7 @@ echo
 printf "Please wait for container install to finish"
 
 for i in $(seq 1 40); do
-    if [ "$(docker inspect -f "{{.State.Health.Status}}" $(name))" == "healthy" ] ; then
+    if [ "$(docker inspect -f "{{.State.Health.Status}}" "$name")" == "healthy" ] ; then
         printf ' OK'
     else
         sleep 2
