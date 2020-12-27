f6913974d0dcc247f79f6152cc71db11088fba2a
Benjamin Lenau Hansen <benjamin@lenau.dk>
2020-12-27 20:30:15 +0100

    Update adlists.sh
    Changed some formatting and commented all relevant lines for better understanding.

--- adlists.sh
+++ adlists.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# update pihole
+# Export already added adlists
 sqlite3 /etc/pihole/gravity.db "SELECT Address FROM adlist" |sort >pihole.list
 
-# uncomment the following lines if you want more adlists added
-#wget -qO - https://v.firebog.net/hosts/lists.php?type=tick |sort >>temp.list
+# Uncomment the following lines if you want more adlists added
+# The folloing wget-line is special because it first needs to be downloaded and sorted before being in a format that Pi-hole accepts
+#wget -qO - https://v.firebog.net/hosts/lists.php?type=tick | sort >>temp.list
 #echo "https://block.energized.pro/ultimate/formats/hosts.txt" >>temp.list
-
 echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" >>temp.list
-echo "https://mirror1.malwaredomains.com/files/justdomains" >>temp.list
 echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" >>temp.list
 sort temp.list >all.list
-comm -23 pihole.list all.list |xargs -I{} sudo sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist WHERE Address='{}';"
-comm -13 pihole.list all.list |xargs -I{} sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (Address,Comment,Enabled) VALUES ('{}','Script added `date +%F`',1);"
+
+# Add adlists to Pi-hole database
+comm -23 pihole.list all.list | xargs -I{} sudo sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist WHERE Address='{}';"
+comm -13 pihole.list all.list | xargs -I{} sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (Address,Comment,Enabled) VALUES ('{}','Script added `date +%F`',1);"
+
+# Cleanup
 rm all.list temp.list pihole.list
+
+# Add regex entries from mmotti
+#apt install -y python3
+#curl -sSl https://raw.githubusercontent.com/mmotti/pihole-regex/master/install.py | sudo python3
+
+# Restart and Reload Pi-hole contents
 pihole restartdns reload-lists
 pihole -g
