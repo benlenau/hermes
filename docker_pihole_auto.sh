37184f062580bc94a07d4ddee6b55ec3342250c0
Benjamin Lenau Hansen <benjamin@lenau.dk>
2022-01-07 13:46:27 +0100

    reverted to no password

--- docker_pihole_auto.sh
+++ docker_pihole_auto.sh
@@ -42,7 +42,7 @@ docker run -d \
 	--dns=1.0.0.1 \
         --restart=unless-stopped \
 	--hostname=$name \
-        -e WEBPASSWORD="hermes" \
+        -e WEBPASSWORD="" \
 	-e DNSMASQ_USER=$DNSMASQ_USER \
 	-e VIRTUAL_HOST=$name \
 	-e PIHOLE_DNS_="$DNS1;$DNS2" \
