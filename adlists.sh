0d39010eeaffa81c41227e39f1d6b598bab415cf
Benjamin Lenau Hansen <benjamin@lenau.dk>
2022-06-13 15:52:05 +0200

    Whitelist Instagram

--- adlists.sh
+++ adlists.sh
@@ -27,6 +27,7 @@ rm all.list temp.list pihole.list
 # Add to whitelist
 pihole --white-regex "(\.|^)microsoft\.com$" "(\.|^)gvt3\.com$" "(\.|^)gvt2\.com$" "(\.|^)gstatic\.com$" "(\.|^)youtube\.com$" "(\.|^)ui\.com$" "(\.|^)msecnd\.net$"
 pihole -w video-fa.scdn.co --comment "Spotify Podcasts"
+pihole -w i.instagram.com --comment "Instagram"
 
 # Add to blacklist
 pihole --regex ".ru$" ".work$" ".fit$" ".casa$" ".loan$" ".cf$" ".tk$" ".rest$" ".ml$" ".london$" ".top$" ".live$" ".ga$" ".buzz$" ".date$"
