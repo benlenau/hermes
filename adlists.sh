46d34dc011e29ac35b9b1c47384624e2c75b95a2
Benjamin Lenau Hansen <benjamin@lenau.dk>
2022-07-17 17:03:43 +0200

    added l.instagram.com

--- adlists.sh
+++ adlists.sh
@@ -31,6 +31,7 @@ pihole -w i.instagram.com --comment "Instagram"
 pihole -w amp-api-edge.apps.apple.com --comment "Apple App Store"
 pihole -w www.nfbio.dk --comment "Nordisk Film Biografer"
 pihole -w t.co --comment "Twitter Links"
+pihole -w l.instagram.com --comment "Instagram Links (app)"
 
 # Add to blacklist
 pihole --regex ".ru$" ".work$" ".fit$" ".casa$" ".loan$" ".cf$" ".tk$" ".rest$" ".ml$" ".london$" ".top$" ".live$" ".ga$" ".buzz$" ".date$"
