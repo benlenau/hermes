99f08ddc39c570af63ad734ba2a72a7e21fbe99d
Benjamin Lenau Hansen <benjamin@lenau.dk>
2022-10-02 20:22:47 +0200

    Update lite_docker_hermes.sh

--- lite_docker_hermes.sh
+++ lite_docker_hermes.sh
@@ -65,6 +65,7 @@ docker exec pihole pihole --white-regex \
 	"(\.|^)ui\.com$" \
 	"(\.|^)youtube\.com$" \
 	"(\.|^)app-measurement\.com$" \
+	"(\.|^)roblox\.com$" \
 	--comment "Hermes Lite Default" >/dev/null 2>&1
 
 # Run Pi-hole DNS restart inside Docker container to make install changes permanent.
